be6288cf2d808635e1b83a1f5d46b55486452252
Elvisthegreat <>
2024-01-28 18:58:48 +0000

    Added an eventListener for run check button

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,7 @@ const API_URL = "https://ci-jshint.herokuapp.com/api";
 const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
 
 document.getElementById("status").addEventListener("click", e => getStatus(e));
+document.getElementById('submit').addEventListener('click', e => postForm(e));
 
 async function getStatus(e) {
 
@@ -14,20 +15,22 @@ async function getStatus(e) {
 
     if (response.ok) {
         displayStatus(data);
-    }else{
+    } else {
         throw new Error(data.error);
     }
 
 }
 
-function displayStatus(data){
-    
-    let heading = 'API Key Status';
+function displayStatus(data) {
+
+    let heading = "API Key Status";
     let results = `<div>Your key is valid until</div>`;
     results += `<div class="key-status">${data.expiry}</div>`;
 
-    document.getElementById('resultsModalTitle').innerText = heading;
-    document.getElementById('results_content').innerText = results;
-
+    document.getElementById("resultsModalTitle").innerText = heading;
+    document.getElementById("results-content").innerHTML = results;
     resultsModal.show();
+
 }
+
+async function postForm(e){}
